205bcb6218c3c0460bcee853bcc0bdce814dab9e
kangkang088 <1808131844@qq.com>
2022-10-30 16:40:12 +0800

    更新

--- Project1/Project1/源.c
+++ Project1/Project1/源.c
@@ -44,9 +44,9 @@
 //	printf("%c\n", arr);//出现A
 //	return 0;
 //%c表示输出一个字符；
-
+//
 //%d表示输出带符号的十进制整型数；
-
+//
 //%s表示输出一个字符串，知道遇到“\0”。
 //int main()		
 //{
@@ -76,7 +76,7 @@
 //
 //	return 0;
 //}
-
+//
 //int MAX(int x, int y)
 //{
 //	if (x > y)
@@ -139,16 +139,119 @@
 //		printf("%d\n", c);
 //	return 0;
 //}
+///*int main()
+//{
+//	printf("年薪100万,我要敲代码十万行\n");
+//	int i = 0;
+//	while (i <100000)
+//	{
+//		i++;
+//		printf("%d\n", i);
+//		
+//	}
+//	printf("成功实现目标");
+//	return 0;
+//}*/
+//register
+//signed
+//unsigned
+//typedef 类型定义，又叫类型重定义
+//int main()
+//{
+//	typedef  signed int s_int;
+//		int a = 5;
+//		s_int b = 5;
+//		//typedef unsigned int u_int;
+//	//unsigned int num = 20;
+//	//u_int num2 = 58;
+//	return 0;
+//}
+//void chance()
+//{
+//	 static int a = 0;//此时局部变量a的生命周期变长，
+//	a++;
+//	printf("%d\n", a);
+//}
+//int main()
+//{
+//static修饰局部变量，使其生命周期变长；修饰全局变量，是其作用域变小，只能在自己所在的文件夹内部使用。
+//	int i = 0;
+//	while (i < 5)
+//	{
+//		chance();
+//		i++;
+//	}
+//	return 0;
+//}
+//
+//int main()
+//{
+//	extern int g_val;//extern是声明外部符号的；
+//	printf("%d\n", g_val);//static 修饰源2中的全局变量，此时报错。
+//	return 0;
+//}
+///*extern int Add(int, int);*///static 修饰函数也会改变作用域（不准确的说法）；其实是改变了函数的链接属性；
+//普通函数具有外部链接属性；如果被static修饰，其属性会变为内部链接属性。
+//int main()
+//{
+//	int a = 50;
+//	int b = 65;
+//	int sum = Add(a, b);
+//	printf("%d\n", sum);
+//	return 0;
+//}
+//#define MAX 100//#define定义的标识符常量
+////#define可以定义宏—参数
+//int main()
+//{
+//	int a = MAX;
+//	return 0;
+//}
+//函数的方法
+//int MAX(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+////宏的定义方式
+//#define MAX(X,Y) (X>Y?X:Y)
+//int main()
+//{
+//	int a = 84;
+//	int b = 45;
+//	//函数
+//	int c = MAX(a, b);
+//	printf("%d\n", c);
+//	//宏
+//	c = MAX(a, b);
+//	printf("%d\n", c);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 10;
+//	printf("%p\n", &a);//输出a的地址    &取地址操作符    a&b,按位与    |按位或    ^按位异或    ~按位取反
+//	int *p = &a;
+//	printf("%p\n", p);//指针变量p用来存放地址，*是解引用操作符，*p是指根据p中的地址进行解引用，找到该地址对应的变量a；
+//	//此时*p找到变量，*p = 20；则变量变为20。
+//	*p = 20;
+//	printf("%d\n", a);//a变为20
+//	char arr = 'A';
+//	printf("%p\n", &arr);
+//	char *pt = &arr;
+//	*pt = 'K';
+//	printf("%c\n", arr);
+//	return 0;
+//}
 int main()
 {
-	printf("年薪100万,我要敲代码十万行\n");
-	int i = 0;
-	while (i <100000)
-	{
-		i++;
-		printf("%d\n", i);
-		
-	}
-	printf("成功实现目标");
+	int a = 50;
+	int* p = &a;
+	printf("%p\n", &a);
+	*p = 15;
+	printf("%d\n", a);
+	printf("%d\n", sizeof(p));
 	return 0;
 }
