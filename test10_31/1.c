c5d4ab5263329b2fe0ca2edfd31c41a84e08e0da
kangkang088 <1808131844@qq.com>
2022-10-31 17:25:37 +0800

    更新

--- test10_31/1.c
+++ test10_31/1.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+#include <string.h>
 //int main()
 //{
 //	printf("%d\n", sizeof(char*));
@@ -8,27 +9,28 @@
 //	printf("%d\n", sizeof(float*));
 //	return 0;
 //}
-struct BOOK
-{
-	char name[20];
-	short price;
-};
-int main()
-{
-	struct BOOK a = { "c语言",25 };
-
-	struct BOOK* b = &a;
-	printf("%s\n", b->name);
-	printf("%d\n", b->price);
-	a.price = 2;
-	printf("%d\n", a.price);
-	printf("%s\n", a.name);
-
-
-
-	/*printf("%s\n", a.name);
-	printf("%d元\n", a.price);
-	a.price = 38;
-	printf("修改之后的价格：%d元\n", a.price);*/
-	return 0;
-}
+//struct BOOK
+//{
+//	char name[20];
+//	short price;
+//};
+//int main()
+//{
+//	struct BOOK a = { "c语言",25 };
+//
+//	struct BOOK* b = &a;
+//	printf("%s\n", b->name);
+//	printf("%d\n", b->price);
+//	a.price = 2;
+//	strcpy(a.name, "c++");
+//	printf("%d\n", a.price);
+//	printf("%s\n", a.name);
+//
+//
+//
+//	/*printf("%s\n", a.name);
+//	printf("%d元\n", a.price);
+//	a.price = 38;
+//	printf("修改之后的价格：%d元\n", a.price);*/
+//	return 0;
+//}
