f39369bcac500e44e1b5536f86c9dcd724c80a22
kangkang088 <1808131844@qq.com>
2022-11-08 22:09:16 +0800

    new .

--- 11-7/11-7/add.h
+++ 11-7/11-7/add.h
@@ -6,12 +6,12 @@
 
 //实际工程中
 
-//#ifndef __ADD_H__
-//#define __ADD_H__
+//#ifndef __ADD_H__  //如果没有引用，
+//#define __ADD_H__   //引用
 //
 ////函数声明
 //int add(int x, int y);
 //
 //
-//#endif
+//#endif  //结束
 
