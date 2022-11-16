e28d0bb1c007667612b58dc6dd96f0bc65d0f3e7
kangkang088 <1808131844@qq.com>
2022-11-16 19:47:43 +0800

    new

---
+++ 11-16/11/11-16.C
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//依次输入一个学生的3科成绩，在屏幕上输出该学生的总成绩以及平均成绩。
+//int main()
+//{
+//	float a = 0;
+//	float b = 0;
+//	float c = 0;
+//	scanf("%f%f%f", &a, &b,&c);
+//	float x = 0;
+//	float y = 0;
+//	x = a + b + c;
+//	y = x / 3;
+//	printf("%.2f %.2f", x, y);
+//	return 0;
+//}
+
+
+//求BMI
+//int main()
+//{
+//	int weight = 0; 
+//	int tall = 0;
+//	scanf("%d%d", &weight, &tall);
+//	float BMI = 0;
+//	BMI = weight*1.0/tall*1.0/tall * 100 * 100;
+//	printf("%.2f", BMI);
+//	return 0;
+//}
+
+
+//
